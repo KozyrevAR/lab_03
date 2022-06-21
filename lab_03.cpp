8917ae675570daeea5b1d89d93af42bc74848380
Kozyrev (Koryrev A.R.) <KozyrevAR@mpei.ru>
2022-06-22 01:58:17 +0300

    до модулей

--- lab_03.cpp
+++ lab_03.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <conio.h>
 #include <vector>
+#include <string>
 using namespace std;
 vector<double>
 input_numbers(size_t count) 
@@ -85,7 +86,51 @@ void show_histogram_text(vector<size_t>&bins, size_t& bin_count, size_t& number_
             cout << '*';
         }
         cout << endl;
+    }   
+}
+void
+svg_begin(double width, double height) 
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void
+svg_end() 
+{
+    cout << "</svg>\n";
+}
+void
+svg_text(double left, double baseline, string text) 
+{
+    cout << "<text x='" << left << "' y='" << baseline <<"' >"<< text <<"</text>";
+}
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
+{
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "'/>";
+}
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    double top = 0;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
     }
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    svg_end();
     
 }
 int main()
@@ -105,7 +150,8 @@ int main()
     double max;
     find_minmax(numbers, min, max);
     make_histogram(bin_count, number_count, numbers, bins, min, max);
-    show_histogram_text(bins, bin_count, number_count);
+    show_histogram_svg(bins);
+   
     return 0;
 }
 
